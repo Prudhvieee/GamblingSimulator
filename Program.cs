84499cb8832f6d7b157fab2243d3fbe8b75415cf
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-03 09:58:15 +0530

    [Prudhvi] add .  declaring variables

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace GamblingSimulator
 {
     public class Program
     {
+        //Declaring initial Variables
+        private static int moneyAtStart = 100;
+        private static int betAmt = 1;
+        private static int gamblingMoney = moneyAtStart;
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Gambling Simulator");
