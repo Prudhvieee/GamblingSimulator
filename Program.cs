136775669445454c635a3790950b3d961e321e88
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-03 10:31:30 +0530

    [Prudhvi] add . betting money

--- Program.cs
+++ Program.cs
@@ -11,6 +11,26 @@ namespace GamblingSimulator
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Gambling Simulator");
+            gambling();
+        }
+        /// <summary>
+        /// UC-2
+        /// This method generates random value and calculates the gambler win or loose
+        /// </summary>
+        public static void gambling()
+        {
+            Random random = new Random();
+            var n = random.Next(0, 2);
+            if (n == 1)
+            {
+                gamblingMoney += betAmt;
+                Console.WriteLine("won:" + " " + gamblingMoney + " " + moneyAtStart);
+            }
+            else if (n == 0)
+            {
+                gamblingMoney -= betAmt;
+                Console.WriteLine("Lost:" + " " + gamblingMoney + " " + moneyAtStart);
+            }
         }
     }
 }
