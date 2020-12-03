819ed5e724974007dac45860cec076853e1fc0f2
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-03 09:57:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GamblingSimulator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambling Simulator");
+        }
+    }
+}
